f44a1499dde2ed0b5904964d3468f7c79cc577c7
techno-cat <techno.cat.miau@gmail.com>
2011-07-10 23:18:24 +0900

    sin波出力部の見直し

--- SandBox/sin.pl
+++ SandBox/sin.pl
@@ -9,6 +9,8 @@ exit(0);
 sub main {
 	my $file_name = "sin.wav";
 	my $sf = 44100; # サンプリング周波数
+	my $bitsPerSample = 8; # 量子化ビット数
+
 	my $time = 1.0; # 音の長さ(sec)
 	my $size = int($sf * $time);
 
@@ -24,7 +26,7 @@ sub main {
 		. pack('L', 44100) # samplesPerSec
 		. pack('L', 1 * 44100) # bytesPerSec = channel * samplesPerSec
 		. pack('S', 1) # blockSize
-		. pack('S', 8); # bitsPerSample
+		. pack('S', $bitsPerSample); # bitsPerSample
 	my $data_chunk =
 		  'data' # chunkID
 		. pack('L', $size); # chunkSize
@@ -40,17 +42,32 @@ sub main {
 		# data_chunkのdeta部
 		my $freq = 440.0; # ラの音
 		my $interval = $sf / $freq;
-		my $dt = 0.0;
+
+		my $osc = sub { 0; };
+		if ( $bitsPerSample == 8 ) {
+			# 量子化ビット数が8bitの場合
+			# 最小0, 最大255, オフセット128
+			$osc = sub {
+				my $t = shift;
+				my $rad = (2.0 * pi()) * ($t / $interval);
+				my $val = int(sin($rad) * 127.0) + 128;
+				return pack( 'C', $val );
+			};
+		}
+		else {
+			# 量子化ビット数が16bitの場合
+			# 最小-32768, 最大32767, オフセット0
+		}
+
+		my $t = 0.0;
 		for (my $i=0; $i<$size; $i++) {
-			if ( $interval < $dt ) {
-				$dt -= $interval;
+			if ( $interval < $t ) {
+				$t -= $interval;
 			}
 
-			my $rad = (2.0 * pi()) * ($dt / $interval);
-			my $val = int(sin($rad) * 127.0)  + 127;
-			print $fh pack('C', $val );
+			print $fh $osc->( $t );
 
-			$dt += 1.0;
+			$t += 1.0;
 		}
 
 		$fh->close();
