7a7b7936b3fd7f8ff45cbae5e65880713a1c0c22
techno-cat <techno.cat.miau@gmail.com>
2011-09-19 21:30:49 +0900

    edit for MacBookAir.

--- p5-NeSynth/example/05.techno.pl
+++ p5-NeSynth/example/05.techno.pl
@@ -22,16 +22,25 @@ my $snare = {
 		waveform => 'sin',
 		mod => { speed => 0.10, depth => 8.0, waveform => 'noise' }
 	},
-	amp => { sec => 0.3, waveform => 'env', curve => 2.7 }
+	amp => { sec => 0.2, waveform => 'env', curve => 2.7 }
 };
 
-my $hat = {
+my $o_hat = {
 	osc => {
 		freq => 8000,
 		waveform => 'sin',
 		mod => { speed => 0.06, depth =>  2.0, waveform => 'noise' }
 	},
-	amp => { sec => 0.4, waveform => 'env', curve => 2.7 }
+	amp => { sec => 0.2, waveform => 'env', curve => 2.7 }
+};
+
+my $c_hat = {
+	osc => {
+		freq => 8000,
+		waveform => 'sin',
+		mod => { speed => 0.06, depth =>  2.0, waveform => 'noise' }
+	},
+	amp => { sec => 0.15, waveform => 'env', curve => 2.7 }
 };
 
 
@@ -39,9 +48,10 @@ my $synth = new Sound::NeSynth->new();
 $synth->render({
 	bpm => 138, # beats per minute
 	beats => [
-		{ seq => [ 1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0 ], tone => $kick , vol => 1.0 },
-		{ seq => [ 0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0 ], tone => $snare, vol => 0.5 },
-		{ seq => [ 0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0 ], tone => $hat  , vol => 0.4 }
+		{ seq => [ 1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0 ], tone => $kick , vol => 1.00 },
+		{ seq => [ 0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0 ], tone => $snare, vol => 0.05 },
+		{ seq => [ 0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0 ], tone => $o_hat, vol => 0.020 },
+		{ seq => [ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 ], tone => $c_hat, vol => 0.012 }
 	]
 });
 
