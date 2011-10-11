ae5e39b5e0ced03316ac59e0e0c6f04cceed9348
techno-cat <techno.cat.miau@gmail.com>
2011-10-12 01:46:23 +0900

    配列の要素数算出時にintをつけてなかったので修正, attackを実装

--- p5-NeSynth/lib/Sound/NeSynth.pm
+++ p5-NeSynth/lib/Sound/NeSynth.pm
@@ -79,10 +79,32 @@ sub _create_oneshot {
 		die '"amp" not found in arguments.'
 	}
 
+	# sec廃止までの暫定処理
+	my $amp = $arg_ref->{amp};
+	my $ar = {
+		attack => ( exists $amp->{attack} ) ? $amp->{attack} : 0.0,
+		release => ( exists $amp->{release} ) ? $amp->{release} : 0.0
+	};
+	$ar->{release} = $amp->{sec} - $ar->{attack};
+
 	my $osc = create_modulator( $samples_per_sec, $arg_ref->{osc} );
 	my $env = create_modulator( $samples_per_sec, $arg_ref->{amp} );
-	my $cnt = $samples_per_sec * $arg_ref->{amp}->{sec};
-	my @samples = map{ $osc->() * $env->(); } 1..$cnt;
+	
+	my @samples = ();
+	my $attack = int($samples_per_sec * $ar->{attack});
+	my $release = int($samples_per_sec * $ar->{release});
+
+	if ( 0 < $attack ) {
+		push @samples, map {
+			$osc->() * ( ($_ / $attack) ** 2.0 );
+		} 0..($attack - 1);
+	}
+
+	if ( 0 < $release ) {
+		push @samples, map {
+			$osc->() * $env->();
+		} 0..($release - 1);
+	}
 
 	return \@samples;
 }
